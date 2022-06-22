1895bf09fd667a3dbf627137ee300d5c681e3b16
Shivam <thisisshivam06@gmail.com>
2022-06-22 19:24:25 +0530

    String Reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+    debugger;
+  }
+  return reversed;
+}
+
+reverse("asdf");
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
